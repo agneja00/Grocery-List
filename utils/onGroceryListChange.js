9ad8c1402ed481e55c89122364d616289327cd3e
agneja00 <agnieshaa00@gmail.com>
2023-06-26 20:50:50 +0300

    Grocery list app Done

--- utils/onGroceryListChange.js
+++ utils/onGroceryListChange.js
@@ -6,7 +6,6 @@ const removeAllBtn = document.querySelector('.remove-all');
 export const onGroceryListChange = () => {
     const groceries = getDataFromLocalStorage(localStorageKeys.groceries);
     if (groceries.length) {
-        console.log(groceries.length)
         removeAllBtn.removeAttribute("disabled");
     } else {
         removeAllBtn.setAttribute('disabled', true);
